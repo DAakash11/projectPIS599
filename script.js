3186748c1c39810a1215ce6310f4d564a90e6ce3
Aakash Dhakal <130034550+DAakash11@users.noreply.github.com>
2024-11-15 18:33:17 +0000

    show room from array

--- script.js
+++ script.js
@@ -18,29 +18,12 @@ function showRooms (dataSet) {
                 <h2>${room.name}</h2>
                 <p>Room booked by £${room.bookedBy} per night</p>
                 <p>Price: £${room.price} per night</p>
-                <button onclick="bookRoom('${room.name}', ${room.price})"> Book Now </button>
-                <button onclick="editRoom(${room.roomIndex})"> Edit Room </button>
-                <button onclick="deleteRoom(${room.roomIndex})"> Delete Room </button>
+                <button onclick="bookRoom('${index})"> Book Now </button>
+                <button onclick="editRoom(${index})"> Edit Room </button>
+                <button onclick="deleteRoom(${index})"> Delete Room </button>
             `;
         });
     }
 }
 
 showRooms(dataHolder);
-
-// Add or Update a room
-
-function addRoom() {
-    const roomName = document.getElementById('room-name').value;
-    const roomBookedBy = document.getElementById('room-bookedBy').value;
-    const roomPrice = document.getElementById('room-price').value;
-    room.push({ name: roomName, bookedBy: roomBookedBy, price: parseInt(roomPrice)});
-    if (editingRoomId === null) {
-    } else {
-        rooms[editingRoomId] = { name:roomName, bookedBy: roomBookedBy, price: parseInt(roomPrice)};
-        editingRoomId = null;
-        document.getElementById('add-update-btn').value = 'Add Room';
-    }
-    showRooms();
-}
-
