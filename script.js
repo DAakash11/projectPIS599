18402b93a4dc2964e84a3f7c6e2538c72e8a37ec
Aakash Dhakal <130034550+DAakash11@users.noreply.github.com>
2024-11-16 14:46:49 +0000

    creating element miss-match

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let dataHolder = [
     {
         name: "room zero",
         bookedBy: "Hello Mom",
-        price: 1234
+        price: 2500
     }
 ];
 
@@ -12,10 +12,11 @@ let dataHolder = [
 function showRooms () {
     const roomContainer = document.getElementById('room-container');
     dataHolder.forEach((room, index) => {
-        const roomElement = document.createElement('room');
+        const roomElement = document.createElement('div');
         roomElement.innerHTML = `
             <h2>${room.name}</h2>
             <p>Booked by <strong>${room.bookedBy}</strong></p>
+            <p>Price: <strong>${room.price}</strong></p>
             <button onclick="bookRoom()">Book Now</button>
             <button onclick="btnEdit()">Edit</button>
             <button onclick="btnDelete()">Delete</button>
