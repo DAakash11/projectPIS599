84b046587606afde3a5b59b1d2731ee66cd4d9a5
Aakash Dhakal <130034550+DAakash11@users.noreply.github.com>
2024-11-18 20:19:52 +0000

    comments and new function addRoom()

--- script.js
+++ script.js
@@ -3,29 +3,56 @@ let dataHolder = [
         name: "room zero",
         bookedBy: "Hello Mom",
         price: 2500
+    },
+    {
+        name: "room zero",
+        bookedBy: null,
+        price: 2500
     }
 ];
 editingRoomId = null;
 
-// Display Rooms
+// function to add Room details
+
+function addRoom() {
+    const custName = document.getElementById('room-bookedBy');
+    const roomName = document.getElementById('room-name');
+    const roomPrice = document.getElementById('room-price');
+    dataHolder.push({ bookedBy: custName, name: roomName, price:roomPrice});
+}
+
+
+// function to display Rooms
 
 function showRooms () {
     const roomContainer = document.getElementById('room-container');
     dataHolder.forEach((room, index) => {
         const roomElement = document.createElement('div');
-        roomElement.innerHTML = `
-            <h2>${room.name}</h2>
-            <p>Booked by <strong>${room.bookedBy}</strong></p>
-            <p>Price: <strong>${room.price}</strong></p>
-            <button onclick="bookRoom()">Book Now</button>
-            <button onclick="btnEdit()">Edit</button>
-            <button onclick="btnDelete()">Delete</button>
-            `;
+        if (room.bookedBy == null) {
+            roomElement.innerHTML = `
+                <h2>${room.name}</h2>
+                <p>Price: <strong>${room.price}</strong></p>
+                <button onclick="bookRoom()">Book Now</button>
+                <button onclick="btnEdit()">Edit</button>
+                <button onclick="btnDelete()">Delete</button>
+                `;
+        } else {
+            roomElement.innerHTML = `
+                <h2>${room.name}</h2>
+                <p>Booked by <strong>${room.bookedBy}</strong></p>
+                <p>Price: <strong>${room.price}</strong></p>
+                <button onclick="alert('It is not available at the moment...')">Unavaiable</button>
+                <button onclick="btnEdit()">Edit</button>
+                <button onclick="btnDelete()">Delete</button>
+                `;
+        }
         roomContainer.append(roomElement);
     });
 }
 
 showRooms();
+
+// function for editing details
 
 function btnEdit(roomIndex) {
     const room = dataHolder[roomIndex];
@@ -35,6 +62,8 @@ function btnEdit(roomIndex) {
     document.getElementById('add-update-btn').textContent = 'Update Details';
     editingRoomId = roomIndex;
 }
+
+// function for deleting details
 
 function btnDelete(roomIndex) {
     room.splice(roomIndex, 1);
